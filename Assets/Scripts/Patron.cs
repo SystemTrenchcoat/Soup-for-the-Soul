74cab96efd6b627a68355d04da4596674230113f
Asteros Irons <Elijahairons@gmail.com>
2024-07-16 14:11:13 -0400

    Added basic functions for patron

--- Assets/Scripts/Patron.cs
+++ Assets/Scripts/Patron.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Patron : MonoBehaviour
 {
+    public float timer = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,35 @@ public class Patron : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 0)
+        {
+            AcceptOrder();
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    public void Appear()
+    {
+
+    }
+
+    public void Spawn()
+    {
+
+    }
+
+    public void GiveOrder()
+    {
+
+    }
+
+    public int AcceptOrder()
+    {
+        int score = 0;
+
+
+
+        return score;
     }
 }

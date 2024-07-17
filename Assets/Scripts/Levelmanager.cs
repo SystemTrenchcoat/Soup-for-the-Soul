baf427515423d2ac853030ae6896019ad2bb9b39
Jaden Pfeilstifter <PfeilstifterJ@lakeland.edu>
2024-07-17 14:20:20 -0500

    scene switching should be done

--- Assets/Scripts/Levelmanager.cs
+++ Assets/Scripts/Levelmanager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Levelmanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void level1()
     {
-        
+        SceneManager.LoadScene("Area1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void level2()
     {
-        
+        SceneManager.LoadScene("Area2");
+    }
+    
+    public void level3()
+    {
+        SceneManager.LoadScene("Area3");
     }
 }

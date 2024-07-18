ba717a230e2fe5b994d562d6522aa12926b4fc03
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-07-18 14:25:55 -0400

    Added more food and ingredients

--- Assets/Scripts/Ingredients.cs
+++ Assets/Scripts/Ingredients.cs
@@ -7,7 +7,10 @@ public class Ingredients : MonoBehaviour
 {
     [SerializeField] string name;
     [SerializeField] string description;
-    [SerializeField] int Id;
+    [SerializeField] int Id; 
+    /*When an ingredient is added, they will be added to a Hashset
+    The hashset will be compared to the food's recipe list
+    Was going to use the Id variable for this but it's prob useless*/
 
     public string Name {get; set;}
     public string Description {get; set;}

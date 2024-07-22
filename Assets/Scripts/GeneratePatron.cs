b7f00fdf3adf60302fd81b9135027322e9118c44
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-07-22 12:55:59 -0400

    Changes to 'Patron' script

---
+++ Assets/Scripts/GeneratePatron.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePatron : MonoBehaviour
+{
+   private Queue<Customer> patrons = new Queue<Customer>();
+   
+
+}
